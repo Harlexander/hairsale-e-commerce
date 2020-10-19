e481c668e508cc30cf0c7d9d6f4877e9297ff2bf
Harlexander <dunkwualex6@gmail.com>
2020-10-19 22:28:48 +0100

    comment page for all files

---
+++ comment.js
@@ -0,0 +1,5 @@
+//This page uses html5, CSS3, Bottstrap3, FirebaseFirestore and Vanilla Javascript
+//A simple webapp whereby admin can create product and upload them to the userside 
+
+//Uses FirebaseFirestore as storage for images and product details
+//This sitee also use a browser local storage to transfer the product details to the cart and display it 
